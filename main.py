279087328d0051a1d8f24c0b8a8e273910323fe8
Olga Yarosh <O_ll_y_a@mail.ru>
2023-05-08 16:48:47 +0300

    создание интерфейса для вызова классов/методов/функций

---
+++ main.py
@@ -0,0 +1,39 @@
+from scr.config import config
+from scr.data_psg import CreateBD
+from scr.item import APIKey
+
+def main():
+    employers_id = [
+        5923,    # Ренессанс Банк
+        586,    # Банк Русский стандарт
+        3529,    # СБЕР
+        3776,    # ПАО МТС
+        11680,   # АО ДОМ.РФ
+        1740,    # Яндекс
+        10156,   # Интерфакс
+        1102601,  # Группа Самолет
+        2863076,  # Skillbox
+        8620,    # Rambler&Co
+        15478    # VK
+    ]
+    params = config()
+    vac = APIKey()
+    base = CreateBD('Vacancy_HH')
+    base.create_db(params)
+    base.create_vac_table(params)
+
+    data = vac.api('Python', employers_id)
+    base.insert_table(data, params)
+    #
+    # while True:
+    #     for i in range(2):
+    #         data = vac.api('Python', employers_id)
+    #         base.insert_table(data, params)
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()

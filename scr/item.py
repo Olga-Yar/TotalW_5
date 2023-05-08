f182359efb0452ac376c2da31cdf6bfb79b54af4
Olga Yarosh <O_ll_y_a@mail.ru>
2023-05-08 16:48:05 +0300

    создание класса APIKey для подключение к hh.ru

---
+++ scr/item.py
@@ -0,0 +1,25 @@
+import requests
+
+
+class APIKey:
+    def api(self, keyword, employers_id):
+        """
+        Подключение по API к hh.ru
+        :return: вакансии по определенным критериям
+        """
+        url_api = 'https://api.hh.ru/vacancies'
+        params = {
+            "employer_id": tuple(employers_id),
+            "only_with_salary": True,
+            "currency": "RUR",
+            "text": keyword,
+            "per_page": 5,
+            "area": 113,
+            "page": 0
+        }
+
+        if requests.get(url_api, params=params).status_code == 200:
+            return requests.get(url_api, params=params).json()['items']
+        else:
+            return f'Error: {requests.get(url_api, params=params).status_code}'
+

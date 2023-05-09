7dc964dfa522cfc5e68c508b81e7de9d8dcd9182
Olga Yarosh <O_ll_y_a@mail.ru>
2023-05-09 18:33:49 +0300

    реализация класса DBManager

--- scr/data_psg.py
+++ scr/data_psg.py
@@ -37,7 +37,8 @@ class CreateBD:
                 salary_from int,
                 salary_to int,
                 salary_currency varchar(10),
-                responsibility text
+                responsibility text,
+                url text
                 )
                 """)
 
@@ -54,46 +55,86 @@ class CreateBD:
                     cur.execute(
                         """
                         INSERT INTO vacancies (company_id, employer_name, vacancy_name, salary_from, salary_to, 
-                        salary_currency, responsibility)
-                        VALUES (%s, %s, %s, %s, %s, %s, %s)
+                        salary_currency, responsibility, url)
+                        VALUES (%s, %s, %s, %s, %s, %s, %s, %s)
                         """,
                         (vacancy['employer']['id'], vacancy['employer']['name'], vacancy['name'],
                          vacancy['salary']['from'], vacancy['salary']['to'],
-                         vacancy['salary']['currency'], vacancy['snippet']['responsibility'])
+                         vacancy['salary']['currency'], vacancy['snippet']['responsibility'], vacancy['alternate_url'])
                     )
 
 
 class DBManager:
-    def get_companies_and_vacancies_count(self):
+    def get_companies_and_vacancies_count(self, database_name, params):
         """
         Получает список всех компаний и количество вакансий у каждой компании.
         :return:
         """
-        pass
+        with psycopg2.connect(dbname=database_name, **params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                SELECT employer_name, COUNT(*)
+                FROM vacancies
+                GROUP BY employer_name
+                """)
+                return cur.fetchall()
 
-    def get_all_vacancies(self):
+    def get_all_vacancies(self, database_name, limit, params):
         """
         Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
         :return:
         """
-        pass
+        with psycopg2.connect(dbname=database_name, **params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"""
+                SELECT employer_name, vacancy_name, salary_from, salary_to, url
+                FROM vacancies
+                LIMIT {limit}
+                """)
+                return cur.fetchall()
 
-    def get_avg_salary(self):
+    def get_avg_salary(self, database_name, params):
         """
         Получает среднюю зарплату по вакансиям.
         :return:
         """
+        with psycopg2.connect(dbname=database_name, **params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                SELECT AVG(salary_from)
+                FROM vacancies
+                WHERE salary_from IS NOT NULL
+                """)
+                return int(*cur.fetchone())
 
-    def get_vacancies_with_higher_salary(self):
+    def get_vacancies_with_higher_salary(self, database_name, params):
         """
         Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
         :return:
         """
-        pass
+        with psycopg2.connect(dbname=database_name, **params) as conn:
+            with conn.cursor() as cur:
+                cur.execute("""
+                SELECT vacancy_name, salary_from
+                FROM vacancies
+                WHERE salary_from >
+                (
+                    SELECT AVG(salary_from) FROM vacancies
+                    WHERE salary_from IS NOT NULL
+                )
+                """)
+                return cur.fetchall()
 
-    def get_vacancies_with_keyword(self):
+    def get_vacancies_with_keyword(self, database_name, keyword, params):
         """
         Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”.
         :return:
         """
-        pass
+        with psycopg2.connect(dbname=database_name, **params) as conn:
+            with conn.cursor() as cur:
+                cur.execute(f"""
+                SELECT vacancy_name, responsibility
+                FROM vacancies
+                WHERE vacancy_name LIKE '%{keyword}%'
+                """)
+                return cur.fetchall()

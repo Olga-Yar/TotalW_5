6d092c777f4714761b29d24983eb5251aa2112b5
Olga Yarosh <O_ll_y_a@mail.ru>
2023-05-09 15:25:22 +0300

    создание класса DBManager

--- scr/data_psg.py
+++ scr/data_psg.py
@@ -1,5 +1,4 @@
 import psycopg2
-from psycopg2 import errors
 
 
 class CreateBD:
@@ -62,3 +61,39 @@ class CreateBD:
                          vacancy['salary']['from'], vacancy['salary']['to'],
                          vacancy['salary']['currency'], vacancy['snippet']['responsibility'])
                     )
+
+
+class DBManager:
+    def get_companies_and_vacancies_count(self):
+        """
+        Получает список всех компаний и количество вакансий у каждой компании.
+        :return:
+        """
+        pass
+
+    def get_all_vacancies(self):
+        """
+        Получает список всех вакансий с указанием названия компании, названия вакансии и зарплаты и ссылки на вакансию.
+        :return:
+        """
+        pass
+
+    def get_avg_salary(self):
+        """
+        Получает среднюю зарплату по вакансиям.
+        :return:
+        """
+
+    def get_vacancies_with_higher_salary(self):
+        """
+        Получает список всех вакансий, у которых зарплата выше средней по всем вакансиям.
+        :return:
+        """
+        pass
+
+    def get_vacancies_with_keyword(self):
+        """
+        Получает список всех вакансий, в названии которых содержатся переданные в метод слова, например “python”.
+        :return:
+        """
+        pass
